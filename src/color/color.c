34a8d1c1893067bb31f5bb1831129198d391cc8f
Shammel Lee <holler@shammellee.com>
2022-12-09 01:42:14 -0500

    Add `color` module

--- src/color/color.c
+++ src/color/color.c
@@ -1,8 +1,36 @@
 #include <stdio.h>   // printf, fprintf(), sprintf(), scanf(), sscanf(), fgets(), stdin, stdout, stderr
 #include <stdlib.h>  // General utilities: malloc(), calloc(), realloc(), free(), atoi(), exit(), EXIT_SUCCESS, EXIT_FAILURE
 #include <string.h>  // strcmp(), strcpy(), strcat(), memcpy(), memset()
+#include <stdint.h>  // Fixed-width integers: int8_t, int16_t, int32_t, uint8_t, uint16_t, uint32_t
 
 #include "color.h"
+
+struct _color
+{
+  uint32_t value;
+};
+
+struct _color_rgba
+{
+  uint8_t red;
+  uint8_t green;
+  uint8_t blue;
+  uint8_t alpha;
+};
+
+struct _color_hsla
+{
+  uint8_t hue;
+  uint8_t saturation;
+  uint8_t lightness;
+  uint8_t alpha;
+};
+
+struct _color_palette
+{
+  Color * colors;
+  unsigned int length;
+};
 
 void color_range_header_print(color_range * r)
 {
--- src/color/color.h
+++ src/color/color.h
@@ -6,6 +6,11 @@
 
 typedef unsigned int color;
 
+typedef struct _color Color;
+typedef struct _color_rgba ColorRGBA;
+typedef struct _color_hsla ColorHSLA;
+typedef struct _color_palette ColorPalette;
+
 typedef struct _color_range
 {
   char *       name;
@@ -13,6 +18,23 @@ typedef struct _color_range
   color        end;
   unsigned int columns;
 } color_range;
+
+Color color_from_rgb();
+void color_to_rgb();
+Color color_from_hsl();
+void color_to_hsl();
+Color color_from_hex();
+
+ColorPalette color_palette_create();
+void color_palette_add(Color * c, ColorPalette * const p);
+void color_palette_remove(Color * c, ColorPalette * const p);
+Color color_palette_pop(ColorPalette * const p);
+void color_palette_push(Color const * const c, ColorPalette * const p);
+Color color_palette_shift(ColorPalette * const p);
+void color_palette_unshift(Color const * const c, ColorPalette * const p);
+void color_palette_destroy();
+
+
 
 void color_range_header_print(color_range * r);
 void color_print(color c, color text);
